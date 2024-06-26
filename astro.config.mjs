e6376a93f813b3542e944b72eaa57903e60fbb3c
inayath <inayath.edu@gmail.com>
2024-06-26 16:36:20 +0530

    initial docs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,21 +5,14 @@ import starlight from '@astrojs/starlight';
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'My Docs',
+			title: 'CAMP 2024',
 			social: {
-				github: 'https://github.com/withastro/starlight',
+				// github: 'https://github.com/withastro/starlight',
 			},
 			sidebar: [
 				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', link: '/guides/example/' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
+					label: 'Software Installation',
+					autogenerate: { directory: 'SoftwareInstallation' },
 				},
 			],
 		}),
