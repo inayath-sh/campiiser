d56d4aaddca252163b7bcb4efca507ef8c48e689
inayath-sh <inayath.edu@gmail.com>
2024-07-02 17:01:41 +0530

    moose

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
+import starlightThemeRapide from 'starlight-theme-rapide';
 
 // https://astro.build/config
 export default defineConfig({
 	integrations: [
 		starlight({
 			title: 'CAMP 2024',
+			plugins: [starlightThemeRapide()],
 			social: {
 				// github: 'https://github.com/withastro/starlight',
 			},
