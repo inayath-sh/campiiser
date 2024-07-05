5de320adedc51e33982a358abaff338c94370e2e
inayath-sh <inayath.edu@gmail.com>
2024-07-05 16:04:52 +0530

    collins project

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,19 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 import starlightThemeRapide from 'starlight-theme-rapide';
+import remarkMath from 'remark-math';
+import rehypeMathJax from 'rehype-mathjax';
 
 // https://astro.build/config
 export default defineConfig({
+	markdown: {
+		remarkPlugins: [remarkMath],
+		rehypePlugins: [rehypeMathJax],
+	  },
 	integrations: [
 		starlight({
 			title: 'CAMP 2024',
+			customCss:['./src/mathjax.css'],
 			plugins: [starlightThemeRapide()],
 			social: {
 				// github: 'https://github.com/withastro/starlight',
