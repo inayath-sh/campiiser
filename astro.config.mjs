f1a817035ab44e1a2c0d0b922ff596f82433378f
Inayath Shaikh <inayath.edu@gmail.com>
2026-06-18 14:05:05 +0530

    2026 init

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,56 +1,115 @@
+
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
-import starlightThemeRapide from 'starlight-theme-rapide'
-import remarkMath from 'remark-math';
-import rehypeMathJax from 'rehype-mathjax';
+import remarkGfm from 'remark-gfm';
+import rehypeSlug from 'rehype-slug';
+import { unified } from '@astrojs/markdown-remark';
+import starlightThemeNova from 'starlight-theme-nova';
+import tailwindcss from '@tailwindcss/vite';
 
-// https://astro.build/config
 export default defineConfig({
-	markdown: {
-		remarkPlugins: [remarkMath],
-		rehypePlugins: [rehypeMathJax],
-	  },
-	integrations: [
-		starlight({
-			title: 'CAMP 2025',
-			customCss:['./src/mathjax.css'],
-			plugins: [starlightThemeRapide()],
-			sidebar: [
+  vite: {
+    plugins: [tailwindcss()],
+  },
 
+  markdown: unified({
+    remarkPlugins: [remarkGfm],
+    rehypePlugins: [rehypeSlug],
+  }),
 
-				{
-					label: 'General',
-					autogenerate: { directory: 'General' },
-				},
-				{
-					label: 'Software Installation',
-					autogenerate: { directory: 'SoftwareInstallation' },
-				},
-				{
-				label: 'Projects',
-				items: [
-					'projects/project-guidelines',
-					{
-					label: 'Week 1',
-					items: [
-						'projects/w1projectgroups',
-						'projects/w1projectideas',
+  integrations: [
+    starlight({
+      title: 'CAMP 2026',
+      customCss: [
+        './src/mathjax.css',
+        './src/styles/global.css', // Tailwind entry CSS
+      ],
 
-					],
-					},
-										{
-					label: 'Week 2',
-					items: [
-						'projects/w2projectgroups',
-						'projects/w2projectideas',
+      plugins: [
+        starlightThemeNova({
+          nav: [
+            { label: 'Information Booklet', href: '/general/informationbook' },
+            { label: 'Schedule', href: 'https://camp.iiserpune.ac.in/schedule' },
+          ],
+        }),
+      ],
 
-					],
-					},
-				],
-				},
-
-			],
-		}),
-	],
+      sidebar: [
+        {
+          label: 'General',
+          items: [{ autogenerate: { directory: 'general' } }],
+        },
+        {
+          label: 'Software Installation',
+          items: [{ autogenerate: { directory: 'softwareinstallation' } }],
+        },
+      ],
+    }),
+  ],
 });
+
+// import { defineConfig } from 'astro/config';
+// import starlight from '@astrojs/starlight';
+// import remarkGfm from "remark-gfm";
+// import rehypeSlug from "rehype-slug";
+// import { unified } from '@astrojs/markdown-remark';
+// import starlightThemeNova from 'starlight-theme-nova';
+// import tailwindcss from '@tailwindcss/vite'
+
+// // https://astro.build/config
+// export default defineConfig({
+//   markdown: unified({
+//     remarkPlugins: [remarkGfm],
+//     rehypePlugins: [rehypeSlug],
+//   }),
+// 	integrations: [
+// 		starlight({
+// 			title: 'CAMP 2026',
+// 			customCss:['./src/mathjax.css'],
+//             plugins: [starlightThemeNova({nav:[
+//                 { label: 'Information Booklet', href: '/general/informationbook' },
+//                 { label: 'Schedule', href: 'https://camp.iiserpune.ac.in/schedule' },
+//             ],
+//         })],
+//             sidebar: [
+//             {
+//                 label: 'General',
+//                 items: [
+//                 {
+//                     autogenerate: { directory: 'general' },
+//                 },
+//                 ],
+//             },
+//             {
+//                 label: 'Software Installation',
+//                 items: [
+//                 {
+//                     autogenerate: { directory: 'softwareinstallation' },
+//                 },
+//                 ],
+//             },
+//             // {
+//             //     label: 'Projects',
+//             //     items: [
+//             //     'projects/project-guidelines',
+//             //     {
+//             //         label: 'Week 1',
+//             //         items: [
+//             //         'projects/w1projectgroups',
+//             //         'projects/w1projectideas',
+//             //         ],
+//             //     },
+//             //     {
+//             //         label: 'Week 2',
+//             //         items: [
+//             //         'projects/w2projectgroups',
+//             //         'projects/w2projectideas',
+//             //         ],
+//             //     },
+//             //     ],
+//             // },
+//             ],
+// 		}),
+// 	],
+// });
 
